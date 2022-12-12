d2256347f5fef0d9b04ef0fe9d6e7aef080087c2
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2022-12-12 14:51:45 -0300

    meu projeto

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "c096f3af01e9cf4fdfc8d4b219e4647a"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
